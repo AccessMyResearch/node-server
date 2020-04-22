91fb72b0d88d3decae20b407aa049fd1b34c8dfa
Arnie825 <arnietmp@gmail.com>
2020-04-22 14:39:56 -0500

    updated styling on login CSS

--- TemplateWebpage/src/components/ArticleDescription.vue
+++ TemplateWebpage/src/components/ArticleDescription.vue
@@ -1,50 +1,58 @@
 <template>
-    <div>
-        <div class="row main_heading text-black">
-            <div class="publicaion_information">
-                <h2 class="title">This is where the title of the publication title will go here </h2>
-                <h6 class="publication_date text-muted">Publication date: Goes Here</h6>
-                <h6 class="publication_DOI text-muted">Publication DOI: Goes Here</h6>
-                <h5 class="publication_author">Author(s): Name will go here | Name will go here | Name will go Here | Name will go Here</h5>
-            </div>
-        </div>
-        <div class="main_body center">
-            <div class="row abstract_main card text-black">
-              <div class="card-header">
-                Abstract
-              </div>
-              <div class="card-body">
-                <p class="card-text">
-                  Lorem ipsum dolor sit amet, vis et latine feugiat necessitatibus, ne per singulis 
-                  scripserit, ne iuvaret fastidii usu. Illud persequeris nec ad, vim ut consul placerat
-                  mnesarchum. Clita alienum consectetuer id sit. Pro unum tibique officiis at, eu nam 
-                  labore docendi. Alia impedit oporteat eum in, ferri aliquando vis ut.
+    <div id="placeholder">
+      <div class="left_section">
+        <p>Hello</p>
+      </div>
 
-                  Usu nullam principes ne, ad imperdiet voluptatum vim. Te has utinam invenire. Ex 
-                  electram interpretaris his, exerci graeco dignissim ne qui. Expetendis quaerendum usu 
-                  id, nibh appareat eum ei. Ius an noster evertitur, etiam patrioque mea id.
-                </p>
+      <div class="middle_section">
+          <div class="article_information">
+          <div class="row main_heading text-black">
+              <div class="publicaion_information">
+                  <h2 class="title">This is where the title of the publication title will go here </h2>
+                  <h6 class="publication_date text-muted">Publication date: Goes Here</h6>
+                  <h6 class="publication_DOI text-muted">Publication DOI: Goes Here</h6>
+                  <h5 class="publication_author">Author(s): Name will go here | Name will go here | Name will go Here | Name will go Here</h5>
               </div>
-            </div>
-        </div>
-        <div class="min-vh-100 secondary_body center">
-            <div class="row card text-black">
-              <div class="card-header">
-                Add as of right now
+          </div>
+          <div class="main_body center">
+              <div class="abstract_main card text-black">
+                <div class="card-header">
+                  Abstract
+                </div>
+                <div class="abstract card-body">
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet, vis et latine feugiat necessitatibus, ne per singulis 
+                    scripserit, ne iuvaret fastidii usu. Illud persequeris nec ad, vim ut consul placerat
+                    mnesarchum. Clita alienum consectetuer id sit. Pro unum tibique officiis at, eu nam 
+                    labore docendi. Alia impedit oporteat eum in, ferri aliquando vis ut.
+
+                    Usu nullam principes ne, ad imperdiet voluptatum vim. Te has utinam invenire. Ex 
+                    electram interpretaris his, exerci graeco dignissim ne qui. Expetendis quaerendum usu 
+                    id, nibh appareat eum ei. Ius an noster evertitur, etiam patrioque mea id.
+                  </p>
+                </div>
               </div>
-              <div class="card-body">
-                <p class="card-text">
-                  Include in the middle the publication
-                </p>
-                <p>
-                  Publication View PDF
-                </p>
-                <p>
-                  New tab using Adobe to view publication
-                </p>
+          </div>
+          <div class="min-vh-100 secondary_body center">
+              <div class="row card pdf_view text-black">
+                <div class="card-header">
+                  Add as of right now
+                </div>
+                <div class="card-body">
+                  <p class="card-text">
+                    Include in the middle the publication
+                  </p>
+                  <p>
+                    Publication View PDF
+                  </p>
+                  <p>
+                    New tab using Adobe to view publication
+                  </p>
+                </div>
               </div>
-            </div>
-        </div>
+          </div>
+          </div>
+      </div>
     </div>  
 </template>
 
@@ -54,11 +62,30 @@
 
 <style scoped>
 
+#placeholder{
+  display: flex;
+}
+
+  .left_section{
+    width: 10%;
+  }
+
+  .middle_section{
+    width: 80%;
+  }
+
+  .right_section{
+    width: 10%
+  }
+
 .main_heading{
-  padding-top: 20px;
+  padding-top: 50px;
   border-bottom: 1px solid #ddd;
   border-top: 1px solid #ddd;
+  background: #ddd;
   align-items: center;
+  margin: auto;
+  width: 82%;
 }
 
 .main_body{
@@ -69,13 +96,25 @@
     padding-bottom: 20px;
 }
 
-.abstract_main{
-width: 60%;
+.abstract{
+  width: 80%;
+}
 
+.abstract_main{
+width: 50%;
+margin-left: 140px;
+background: #ddd;
 }
 
 .secondary_body{
   padding-top: 50px;
+  width: 80%;
+  margin: auto;
+}
+
+.pdf_view{
+  background: #ddd;
+  height: 800px;
 }
 
 
--- TemplateWebpage/src/components/Login.vue
+++ TemplateWebpage/src/components/Login.vue
@@ -12,7 +12,7 @@
               </div>
               <div class="form-group">
                 <input type="text" class="form-control" placeholder="Password">
-                  <div class="bottom">
+                  <div class="options_check">
                     <div class="form-check">
                       <input class="form-check-input" type="checkbox" id="forgotpassword">
                       <label class="form-check-label" for="forgotpassword">
@@ -24,7 +24,7 @@
                     </div>
                   </div>
                 </div>
-              <button type="submit" class="login d-inline-block float-center btn button-size btn-secondary btn-lg btn-block">Log in</button>
+                <button type="submit" class="login d-inline-block float-center btn button-size btn-secondary btn-lg btn-block">Log in</button>
             </form>
             <div class="sign_up">
               <button type="submit" class="d-inline-block float-center btn button-size btn-secondary btn-lg btn-block">Sign up</button>
@@ -32,27 +32,27 @@
           </div>
           <div class="icons">
             <h6 class="horizontal-line">or</h6>
-              <div class="log-in-buttons">
-                <div class="set-one">
-                  <div class="placeholder">
-                    <button type="button" class="btn button-size btn-success btn-lg btn-block"><i class="logos float-left fab fa-google"></i>Google</button>
-                  </div>
-                  <div class="placeholder">
-                    <button type="button" class="btn button-size btn-primary btn-lg btn-block"><i class="logos float-left fab fa-facebook-square"></i>Facebook</button>
-                  </div>
+            <div class="logo_button_sets">
+              <div class="set-one">
+                <div class="logo_button">
+                  <button type="button" class="google_button btn button-size btn-success btn-lg btn-block"><i class="logos float-left fab fa-google"></i>Google</button>
                 </div>
-                <div class="set-two">
-                  <div class="placeholder">
-                    <button type="button" class="btn button-size btn-primary btn-lg btn-block"><i class="logos float-left fab fa-linkedin"></i>Linkedin</button>
-                  </div>
-                  <div class="placeholder">
-                    <button type="button" class="btn button-size btn-primary btn-lg btn-block"><i class="logos float-left fab fa-researchgate"></i>Research Gate</button>
-                  </div>
+                <div class="logo_button">
+                  <button type="button" class="facebook_button btn button-size btn-primary btn-lg btn-block"><i class="logos float-left fab fa-facebook-square"></i>Facebook</button>
+                </div>
+              </div>
+              <div class="set-two">
+                <div class="logo_button">
+                  <button type="button" class="linkedin_button btn button-size btn-primary btn-lg btn-block"><i class="logos float-left fab fa-linkedin"></i>Linkedin</button>
+                </div>
+                <div class="logo_button">
+                  <button type="button" class="orcid_button btn button-size btn-primary btn-lg btn-block"><i class="logos float-left fab fa-orcid"></i>ORCID</button>
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </div>
   </div>  
 </template>
@@ -62,27 +62,36 @@
 </script>
 
 <style scoped>
+
 .login_card{
   background-color: var(--card-background);
+  border-radius: 15px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -moz-transform: translateX(-50%) translateY(-50%);
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
 }
 
 .button-size{
   width: 200px;
+  margin: auto 5px;
 }
 
 .set-two{
   padding-top: 10px;
 }
 
-.placeholder{
+.logo_button{
   display: inline-block;
 }
 
-.bottom{
+.options_check{
   display: flex;
 }
 
-.bottom div:last-child{
+.options_check div:last-child{
   margin-left: auto;
 }
 
@@ -100,6 +109,38 @@
 
 .sign_up button:hover{
   background-color: rgb(250, 102, 16);
+}
+
+.google_button{
+  background: #0F9D58;
+}
+
+.google_button:hover{
+  background: rgb(15, 131, 75);
+}
+
+.facebook_button{
+  background: #4267B2;
+}
+
+.facebook_button:hover{
+  background: rgb(51, 81, 141);
+}
+
+.linkedin_button{
+  background: #2867B2;
+}
+
+.linkedin_button:hover{
+  background: rgb(29, 75, 131);
+}
+
+.orcid_button{
+  background: #a6ce39;
+}
+
+.orcid_button:hover{
+  background: #809e2c;
 }
 
 .login{
